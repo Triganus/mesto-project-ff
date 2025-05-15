134ab671d52dcdfd5fb85c84d87f5a8f803cc71b
pavel <pavelbosy452@gmail.com>
2025-05-15 18:56:51 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+  // Клонируем шаблон один раз
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  
+  // Находим все необходимые элементы один раз
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  // Устанавливаем значения
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  // Добавляем обработчик удаления
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.append(cardElement);
+});
